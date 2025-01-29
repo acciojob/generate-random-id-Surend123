fc35a4ffa51e4ede94be1ffe0d4e20bb192f4049
Surendra <sky958447@gmail.com>
2025-01-29 18:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	// let big_charlist = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	// let small_charlist ="qwertyuiopasdfghjklzxcvbnm";
+	// let listOfNumber ="0123456789";
+	let charString = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+	for(let i=1;i<=l;i++){
+		res+=charString.charAt(Math.floor(Math.random()*charString.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
